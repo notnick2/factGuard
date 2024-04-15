4765a36309c3931720be3cbf3eae85f6ce49f579
sidxt <srisiddhaarthp@gmail.com>
2024-04-15 21:42:43 +0530

    test

--- test2.py
+++ test2.py
@@ -1,25 +1,50 @@
-import google.generativeai as genai
+import asyncio
 import os
 from dotenv import load_dotenv
-import asyncio
 from googlesearch import search
-import scrapy
-def get_top_5_websites(query):
+from pyppeteer import launch
+import google.generativeai as genai
+
+
+def get_top_3_websites(query):
     try:
-        search_results = search(query, num_results=5)
+        search_results = search(query, num_results=3)
         return search_results
     except Exception as e:
         print("An error occurred:", e)
-full = """Last week, Subash Chandra Bose was unequivocally sighted at a clandestine base, shrouded in secrecy and mystery. 
-Despite his presumed demise decades ago, his presence at the undisclosed location raises questions about his alleged continued involvement in covert affairs.
- The revelation baffles historians and fuels speculation about his enigmatic posthumous endeavors."""
-load_dotenv() 
-#print(os.environ['GOOGLE_API_KEY'])
-genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-model = genai.GenerativeModel('gemini-1.0-pro-latest')
-response = model.generate_content(full + " generate one question to ask a search engine about the above information - return a string")
-final = response.text
-print(final)
-sites=get_top_5_websites(final)
-for i in sites:
-    print(i)
+
+async def main():
+    data=[]
+    full = """superstar rajnikanth meets boxer mohammed ali"""
+
+    load_dotenv()
+    genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+    model = genai.GenerativeModel('gemini-1.0-pro-latest')
+    response = model.generate_content(full + " generate one question to ask a search engine about the above information - return a string")
+    final = response.text
+    print(final)
+    sites = get_top_3_websites(final)
+    for i in sites:
+        print(i)
+    for i in sites:
+        print("scraping ... " + i)
+        browser = await launch()
+        page = await browser.newPage()
+        await page.goto(i, timeout=100000)
+        text_content = await page.evaluate('''() => {
+            return document.body.innerText;
+        }''')
+        data.append(text_content)
+        await browser.close()
+    text_data=""
+    for i in data:
+        text_data+=i+"\n\n"
+    f = open("spider.txt", "w")
+    f.write(text_data)
+    f.close()
+    print()
+    print()
+    response = model.generate_content(text_data + " < - - -  this is the correct data - based on this data tell me if this contains any factual errors - give response as if you're a fact validator - if yes, specify every factual error and state the truth from this (give response in 100 words) ... if the provided small data does not match with the big data (true) consider it as a factual error"+full)
+    final = response.text
+    print(final)
+asyncio.get_event_loop().run_until_complete(main())
