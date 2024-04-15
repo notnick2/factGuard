f04f6fc90833feba07ca83a1d2209d77b2b115fe
sidxt <srisiddhaarthp@gmail.com>
2024-04-15 22:42:56 +0530

    test

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+#uvicorn main:app --reload
 import asyncio
 import os
 from dotenv import load_dotenv
 from googlesearch import search
 from pyppeteer import launch
+from youtube_transcript_api import YouTubeTranscriptApi
 import google.generativeai as genai
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_top_3_websites(query):
     try:
@@ -12,20 +24,27 @@ def get_top_3_websites(query):
         return search_results
     except Exception as e:
         print("An error occurred:", e)
-
-async def main():
+sites_send=[]
+@app.post("/api/guard-the-fact")
+async def run(req_body: dict):
     data=[]
-    full = """superstar rajnikanth does skydiving with bear grylls"""
-
+    print(req_body["url"])
+    index = req_body["url"].find("=")
+    video_id = req_body["url"][index+1:]
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    full=""
+    for i in transcript:
+        full+=i["text"]+" "
+    print(full)
     load_dotenv()
     genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
     model = genai.GenerativeModel('gemini-1.0-pro-latest')
     response = model.generate_content(full + " generate one question to ask a search engine about the above information - return a string")
     final = response.text
-    print(final)
     sites = get_top_3_websites(final)
     for i in sites:
         print(i)
+        sites_send.append(i)
     for i in sites:
         browser = await launch()
         page = await browser.newPage()
@@ -44,6 +63,5 @@ async def main():
     print()
     print()
     response = model.generate_content(text_data + " < - - -  this is the correct data - based on this data tell me if this contains any factual errors - give response as if you're a fact validator - if yes, specify every factual error and state the truth from this (give response in 100 words) ... if the provided small data does not match with the big data (true) consider it as a factual error"+full)
-    final = response.text
-    print(final)
-asyncio.get_event_loop().run_until_complete(main())
+    final = {"message":response.text,"references":sites_send}
+    return final
