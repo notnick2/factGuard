306d17508085750d29f39c6495004f334c2e1dc5
sidxt <srisiddhaarthp@gmail.com>
2024-04-16 20:34:07 +0530

    test

---
+++ app/app/factguard/page.tsx
@@ -0,0 +1,81 @@
+"use client"
+
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+export default function Home() {
+    const[link, set_link] = useState("");
+    const [theme, setTheme] = useState('light');
+    const session = useSession();
+    console.log(session)
+
+    const toggleTheme = () => {
+      setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+    const submit_link=async()=>{
+        const response = await axios.post("http://localhost:8000/api/guard-the-fact",{url:link})
+        console.log(response.data)
+      }
+
+      return (
+        <div className={`h-screen flex flex-col ${theme}`}>
+          
+          <header className="navbar flex justify-around items-center p-4">
+            <div className="flex items-center">
+              <h1 className="text-xl font-bold">
+                Fact<span className="text-[#8DECB4]">Guard</span>
+              </h1>
+            </div>
+            <div className="flex items-center space-x-6">
+              <button
+                className="nav-link hover:text-blue-500"
+                onClick={toggleTheme}
+              >
+                {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
+              </button>
+              <button onClick={()=>signIn("google")} className="nav-link hover:text-blue-500">
+                Login
+              </button>
+              <button onClick={()=>signIn("google")} className="nav-link hover:text-blue-500">
+                Get Started
+              </button>
+            </div>
+          </header>
+         
+
+          <main className="flex-grow flex justify-center items-center">
+            <div className="max-w-md space-y-4">
+              <h1 className="text-4xl font-bold text-center mb-4">
+                Video Content Factual Verification
+              </h1>
+              <p className="text-xl text-gray-700 text-center mb-4">
+                Enter your YouTube video's link below to quickly start accuracy
+                assessment...
+              </p>
+              
+              <div className="input-container flex flex-row items-center px-4 py-2">
+                <input
+                  type="text"
+                  className="border border-gray-300 rounded p-2 flex-1 mr-4 w-full"
+                  placeholder="Enter URL of YouTube video..."
+                  name="url"
+                  value={link}
+                  onChange={(e) => { set_link(e.target.value) }}
+                  required
+                />
+                <button
+                  onClick={submit_link}
+                  className="bg-black border-[#8DECB4] border-2 text-white px-4 py-2 rounded hover:bg-[#8DECB4] hover:text-black duration-300 transition-all ease-in-out"
+                >
+                  Check Facts
+                </button>
+              </div>
+
+
+            </div>
+          </main>
+        </div>
+      );
+}
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -22,60 +22,11 @@ export default function Home() {
       return (
         <div className={`h-screen flex flex-col ${theme}`}>
           
-          <header className="navbar flex justify-around items-center p-4">
-            <div className="flex items-center">
-              <h1 className="text-xl font-bold">
-                Fact<span className="text-blue-500">Guard</span>
-              </h1>
-            </div>
-            <div className="flex items-center space-x-6">
-              <button
-                className="nav-link hover:text-blue-500"
-                onClick={toggleTheme}
-              >
-                {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
-              </button>
-              <button onClick={()=>signIn("google")} className="nav-link hover:text-blue-500">
-                Login
-              </button>
-              <button onClick={()=>signIn("google")} className="nav-link hover:text-blue-500">
-                Get Started
-              </button>
-            </div>
-          </header>
-         
-
-          <main className="flex-grow flex justify-center items-center">
-            <div className="max-w-md space-y-4">
-              <h1 className="text-4xl font-bold text-center mb-4">
-                Video Content Factual Verification
-              </h1>
-              <p className="text-xl text-gray-700 text-center mb-4">
-                Enter your YouTube video's link below to quickly start accuracy
-                assessment...
+          
+              <p className="text-3xl text-white fixed top-1/2 text-center left-1/2">
+                Under Construction
               </p>
-              
-              <div className="input-container flex flex-row items-center px-4 py-2">
-                <input
-                  type="text"
-                  className="border border-gray-300 rounded p-2 flex-1 mr-4 w-full"
-                  placeholder="Enter URL of YouTube video..."
-                  name="url"
-                  value={link}
-                  onChange={(e) => { set_link(e.target.value) }}
-                  required
-                />
-                <button
-                  onClick={submit_link}
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-                >
-                  Check Facts
-                </button>
-              </div>
 
-
-            </div>
-          </main>
         </div>
       );
 }
